bdb2fd7addb6f7d286d31012b26e897921b174e5
Mahesh361 <vegmahesh@gmail.com>
2021-04-20 23:32:46 -0700

    Fixing webscrapper

--- webScrapper.py
+++ webScrapper.py
@@ -1,15 +1,19 @@
 import requests 
 from selenium import webdriver
 
-file_name = input("Enter manga filename: ")
-with open(file_name, 'r') as f:
+#file_name = input("Enter manga filename: ")
+with open('manga_list.txt', 'r') as f:
 	file_contents = f.readlines()
 
-current_chapters_file = input("Enter chapter filename: ")
-with open(current_chapters_file, 'r') as f:
+#current_chapters_file = input("Enter chapter filename: ")
+current_chapters_file = 'chaps.txt'
+with open('chaps.txt', 'r') as f:
 	current_chapters = f.readlines()
 
-return_file = input("Enter return file: ")
+#return_file = input("Enter return file: ")
+return_file = 'todays_manga.txt'
+
+today_manga_list = []
 
 PATH = '/Users/maheshvegiraju/Desktop/manganelo_Scrapper/manganeloScraper/chromedriver'
 driver = webdriver.Chrome(PATH)
@@ -17,17 +21,18 @@ driver = webdriver.Chrome(PATH)
 for i, url in enumerate(file_contents):
 	driver.get(url)
 	latest_chapter = driver.find_elements_by_xpath('/html/body/div[1]/div[3]/div[1]/div[4]/ul/li[1]/a')[0]
-	chapter_title = latest_chapter.get_attribute('title')
-	chapter_num = chapter_title.split(' ')[-1]
+	chapter_title = latest_chapter.get_attribute('href')
+	chapter_title = chapter_title.split('_')
+	chapter_num = chapter_title[-1]
 	if (chapter_num > current_chapters[i]):
-		with open(current_chapters_file, 'w') as current_chapter:
-			for j, chapter in enumerate(current_chapters):
-				if j == i:
-					print('hi')
-					current_chapter.writelines(chapter_num)
-				else:
-					print('hello')
-					current_chapter.writelines(chapter)
-	with open(return_file, 'w') as f:
-		f.writelines(latest_chapter.get_attribute('href') + '\n')
+		current_chapters[i] = chapter_num
+		today_manga_list.append(latest_chapter.get_attribute('href'))
+with open(return_file, 'w+') as f:
+	f.truncate(0)
+	for link in today_manga_list:
+		f.writelines(link + '\n')
+with open(current_chapters_file, 'w+') as f:
+	f.truncate(0)
+	for chapter in current_chapters: 
+		f.writelines(chapter + '\n')
 
