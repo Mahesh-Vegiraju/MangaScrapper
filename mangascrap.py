e5d69bb0be3bb88fa8d4df933afe84e758979a8b
Mahesh-Vegiraju <vegiraju.mahesh@gmail.com>
2022-07-27 09:15:16 -0700

    added a function to allow for mass opening of bulk new chapters

--- mangascrap.py
+++ mangascrap.py
@@ -26,6 +26,87 @@ def update_file(manga: str, to_write: [str]):
 		for i in to_write:
 			f.write(i)
 
+# Given the name of a manga, this function will open all the chapters new chapters, even if there are multiple
+# new chapters. It also takes in the current chapter number
+def get_new_chapters_bulk(manga: str, current_chapter: str):
+	chaps_to_open = []
+
+	driver = webdriver.Firefox(executable_path="./geckodriver")
+	driver.get("https://mangakakalot.com/")
+	search_bar = driver.find_element_by_class_name("searchi")
+
+	search_bar.send_keys(manga)
+	time.sleep(1.5)
+	result = driver.find_element_by_xpath("//div[@id='search_result']/ul/a[1]").get_attribute("href") # the link of the first result of the search
+
+	search_bar.clear()
+	driver.execute_script("window.open('');")
+	driver.switch_to.window(driver.window_handles[1])
+	driver.get(result)
+
+	time.sleep(2)
+
+	latest_chap = ""
+
+	if "manganato" in result:
+		print("manganato")
+		latest_chap = driver.find_element_by_xpath("//div[@class='panel-story-chapter-list']/ul/li[1]/a").text.split(' ')[1].strip(":")
+	elif "mangakakalot" in result:
+		print("mangakakalot")
+		latest_chap = driver.find_element_by_xpath("//div[@class='chapter-list']/div/span/a[1]").text.split(' ')[1].strip(":") # for mangakakalot
+	elif "manganelo" in result:
+		print("manganelo")
+	else:
+		print("not programmed for this website: " + result)
+		driver.close()
+		sys.exit()
+
+	if latest_chap > current_chapter:
+	# gets the next sequential chapter 
+		i = 0
+		while latest_chap > current_chapter:
+			i += 1 
+			if "manganato" in result:
+				print("manganato")
+				latest_chap = driver.find_element_by_xpath("//div[@class='panel-story-chapter-list']/ul/li[" + str(i) + "]/a").text.split(' ')[1].strip(":")
+				if latest_chap == current_chapter:
+					continue
+				chaps_to_open.append(driver.find_element_by_xpath("//div[@class='panel-story-chapter-list']/ul/li[" + str(i) + "]/a").get_attribute("href")) # adding chapter link to new_chapters
+			elif "mangakakalot" in result:
+				print("mangakakalot")
+				latest_chap = driver.find_element_by_xpath("//div[@class='chapter-list']/div/span/a[" + str(i) + "]").text.split(' ')[1].strip(":") # for mangakakalot
+				if latest_chap == current_chapter:
+					continue
+				chaps_to_open.append(driver.find_element_by_xpath("//div[@class='chapter-list']/div/span/a[" + str(i) + "]").get_attribute("href")) # for mangakakalot
+			elif "manganelo" in result:
+				print("manganelo")
+				print("not implemented yet!")
+				print("skipping over this!")
+
+	for i in chaps_to_open:
+		driver.execute_script("window.open('');") # open new tab
+		driver.switch_to.window(driver.window_handles[-1]) # switch to new tab
+		driver.get(i) # open the link of the first result
+
+	driver.switch_to.window(driver.window_handles[0]) # switch to and close the search tab
+	driver.close()
+
+	driver.switch_to.window(driver.window_handles[0]) # switch to and close the search tab
+	time.sleep(1)
+	driver.close() # closing the manga home page
+	driver.switch_to.window(driver.window_handles[-1]) # switch to first manga chapter to read
+
+		# print(i)
+		# 	if "manganato" in result:
+		# 		print("manganato")
+		# 		latest_chap = driver.find_element_by_xpath("//div[@class='panel-story-chapter-list']/ul/li[" + str(i-1) + "]/a").text.split(' ')[1].strip(":")
+		# 	elif "mangakakalot" in result:
+		# 		print("mangakakalot")
+		# 		latest_chap = driver.find_element_by_xpath("//div[@class='chapter-list']/div/span/a[" + str(i-1) + "]").text.split(' ')[1].strip(":") # for mangakakalot
+		# 	elif "manganelo" in result:
+		# 		print("manganelo")
+
+
 # Takes in a list of lists where the inner list contains the manga name in the first position and 
 # the chapter num in the second and visits mangakakalot and checks if there is any new chapters 
 # that have to be opened and returns a list containing strings to write back to the file
@@ -135,9 +216,10 @@ def visit_website(manga_num: [[str, float]]) -> [str]:
 
 if __name__ == "__main__":
     # new_chap()
-    file = "test.txt"
-    updated_list = visit_website(get_mangas("test.txt"))
-    update_file(file, updated_list)
+    # file = "test.txt"
+    # updated_list = visit_website(get_mangas("test.txt"))
+    # update_file(file, updated_list)
+    get_new_chapters_bulk("Spy X Family", "60")
 
 
             
